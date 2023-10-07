cf74324c44a2610e7cd6467c944709d3c779dcd8
Sergio Peralta <seperalta88@gmail.com>
2023-10-06 23:09:38 -0300

    reactivity

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import ItemList from "./components/ItemList.vue";
+import { ItemInterface } from "./models/items";
+import { reactive } from "vue";
+
+const items: ItemInterface[] = reactive([
+  {
+    id: 1,
+    name: "Item 1",
+    selected: false,
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    selected: true,
+  },
+  {
+    id: 3,
+    name: "Item 3",
+    selected: false,
+  },
+]);
+
+const onSelectItem = (id: number) => {
+  const item = items.find((i) => i.id === id);
+  if (!item) {
+    console.warn(`Item with id ${id} not found`);
+    return;
+  }
+  item.selected = !item.selected;
+};
+</script>
+
+<template>
+  <div class="home">
+    <ItemList :items="items" @selectItem="onSelectItem" />
+  </div>
+</template>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { ItemInterface } from "../models/items";
+defineProps<{ items: ItemInterface[] }>();
+
+const emit = defineEmits<{
+  (e: "selectItem", id: number): any;
+}>();
+
+const handleClick = (item: ItemInterface) => {
+  emit("selectItem", item.id);
+};
+</script>
+
+<template>
+  <div>
+    <h3>Items:</h3>
+    <ul>
+      <li v-for="item in items" :key="item.id" @click="handleClick(item)">
+        {{ item.name }} [{{ item.selected ? "selected" : "not selected" }}]
+      </li>
+    </ul>
+  </div>
+</template>
